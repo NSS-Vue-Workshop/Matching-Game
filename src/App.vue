03881454f58a016cff84bccacfdee6cb751d0899
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-01-17 12:21:49 -0600

    walkthrough

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <matching-game />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MatchingGame from "./components/MatchingGame";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    MatchingGame
   }
 };
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
